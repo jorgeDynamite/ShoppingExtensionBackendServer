c30c4baba2287a543a9d1b4100b60e27bc4dc3af
jorgeDynamite <tobiesongeorge@gmail.com>
2023-03-01 17:48:18 +0100

    changeport

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const cors = require("cors");
 API_KEY = "a69a29600548ceab5d864959e44ed285";
 const request = require("request-promise");
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors({ origin: true }));
 const cheerio = require("cheerio");
